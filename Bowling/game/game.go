5c233250bff1a6fb610e5083f9b523b29e805749
mathieu <mathieu.keller@sda.se>
2024-02-11 11:26:22 +0100

    feat(bowling): add interface for game

---
+++ Bowling/game/game.go
@@ -0,0 +1,22 @@
+package game
+
+type frame struct {
+	PinsRolled []int
+	Score      int
+}
+
+func AddRoll(pins int) {
+
+}
+
+func Frames() []frame {
+	return []frame{}
+}
+
+func TotalScore() int {
+	return 0
+}
+
+func Over() bool {
+	return false
+}
