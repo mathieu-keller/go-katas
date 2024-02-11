54b7c37185edba36ef2b7a6330611e164c683c34
mathieu <mathieu.keller@sda.se>
2024-02-11 14:07:36 +0100

    feat(bowling): finished the implementation

--- Bowling/main.go
+++ Bowling/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mathieu-keller/go-katas/Bowling/game"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	game := game.NewGame()
+	for {
+		fmt.Println("Enter the number of pins knocked down: ")
+		var pins int
+		fmt.Scanln(&pins)
+		game.AddRoll(pins)
+		fmt.Println(game.Frames())
+		fmt.Println(game.TotalScore())
+		if game.Over() {
+			fmt.Println("Game end!")
+			break
+		}
+	}
 }
